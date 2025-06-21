39b9ebaf5fd7edb22deac5955f673b2255e3bd11
Thabata356 <thabatav7@gmail.com>
2025-06-21 01:36:27 -0300

    Continuar a lógica do CRUD e o CSS das páginas de login e da home.

--- pw3-projetoTCC/src/app/app.component.ts
+++ pw3-projetoTCC/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import { HomeComponent } from './home/home.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet, LoginComponent],
-  imports: [HomeComponent],
+  imports: [HomeComponent, LoginComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- pw3-projetoTCC/src/app/home/home.component.ts
+++ pw3-projetoTCC/src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validatio
 
 @Component({
   selector: 'app-home',
-  imports: [ReactiveFormsModule], //CabecalhoComponent
+  imports: [ReactiveFormsModule, CabecalhoComponent], //CabecalhoComponent
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -32,44 +32,73 @@ export class HomeComponent {
     this.criarForms();
   }
 
-  criarForms() {
+criarForms() {
     this.formAdicionar = this.fb.group({
-      remetente: ['', [Validators.required], [Validators.maxLength(50)]],
-      destinatario: ['', [Validators.required], [Validators.maxLength(50)]],
-      descricao: ['', [Validators.required], [Validators.maxLength(450)]],
-      data: [new Date()] // Validar data
+      remetente: ['', [Validators.required, Validators.maxLength(50)]],
+      destinatario: ['', [Validators.required, Validators.maxLength(50)]],
+      descricao: ['', [Validators.required, Validators.maxLength(450)]]
     });
 
     this.formConsultar = this.fb.group({
-      id: ['', [Validators.required]]
+      id: ['', [Validators.required]],
+      remetente: ['', [Validators.required, Validators.maxLength(50)]],
+      destinatario: ['', [Validators.required, Validators.maxLength(50)]],
+      descricao: ['', [Validators.required, Validators.maxLength(450)]]
     });
 
     this.formDeletar = this.fb.group({
-      id: ['', [Validators.required]],
+      id: ['', [Validators.required]]
     });
 
     this.formEditar = this.fb.group({
       id: ['', [Validators.required]],
-      remetente: ['', [Validators.required], [Validators.name], [Validators.maxLength(50)]],
-      destinatario: ['', [Validators.required], [Validators.name], [Validators.maxLength(50)]],
-      descricao: ['', [Validators.required], [Validators.maxLength(450)]],
-      data: [new Date()] // Validar data
+      remetente: ['', [Validators.required, Validators.maxLength(50)]],
+      destinatario: ['', [Validators.required, Validators.maxLength(50)]],
+      descricao: ['', [Validators.required, Validators.maxLength(450)]]
     });
   }
 
-  onAdicionar(){
+  onAdicionar() {
     if (this.formAdicionar.valid) {
-    let novoFeedback = this.formAdicionar.value;
+      const novoFeedback = this.formAdicionar.value;
 
-    this.listaFeedbacks.push(novoFeedback); // push adiciona
+      this.listaFeedbacks.push(novoFeedback);
 
-    // Limpar o formulário para poder inserir dados novos
-    this.formAdicionar.reset();
-  } else {
-    alert('Formulário inválido!'); // Verificar lógica!!
+      this.formAdicionar.reset();
+      alert('Feedback criado com sucesso.');
+    } else {
+      alert('Feedback inválido!');
+    }
   }
+
+  onConsultar() {
+    const id = this.formConsultar.value.id;
+
+    if (id >= 0 && id < this.listaFeedbacks.length) {
+      const feedback = this.listaFeedbacks[id];
+      this.formConsultar.controls['remetente'].setValue(feedback.remetente);
+      this.formConsultar.controls['destinatario'].setValue(feedback.destinatario);
+      this.formConsultar.controls['descricao'].setValue(feedback.descricao);
+      //alert('Remetente: ' + feedback.remetente + '\nDestinatário: ' + feedback.destinatario + '\nDescrição: ' + feedback.descricao);
+    } else {
+      this.formConsultar.controls['remetente'].setValue('');
+      this.formConsultar.controls['destinatario'].setValue('');
+      this.formConsultar.controls['descricao'].setValue('');
+      alert('Nenhum feedback com esse ID.');
+    }
   }
+
+  onDeletar() {
+    const id = this.formDeletar.value.id;
+
+    if (id >= 0 && id < this.listaFeedbacks.length) {
+      this.listaFeedbacks.splice(id, 1); // Remove 1 item no índice "id"
+      this.formDeletar.reset();
+      alert('Feeback foi deletado.')
+    } else {
+      alert('Nenhum feedback com esse ID.');
+    }
+  }
+}
 
   // Falta: Terminar Consultar e fazer Delete e Editar. Arrumar o css e terminar roteamento.
-
-}
